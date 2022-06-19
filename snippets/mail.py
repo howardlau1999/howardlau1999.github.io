1508b6893e1e4f6024a3252fa5e1e83977a6d868
Howard Lau <howardlau1999@hotmail.com>
2022-06-20 00:33:52 +0800

    Update mail.py

--- snippets/mail.py
+++ snippets/mail.py
@@ -9,13 +9,59 @@ import os
 import argparse
 from email.mime.multipart import MIMEMultipart
 import mimetypes
+from base64 import b64encode
+import json
+
+MAILJET_API_V3 = "https://api.mailjet.com/v3/send"
+
 
 def _format_addr(s):
   name, addr = parseaddr(s)
   return formataddr((Header(name, "utf-8").encode(), addr))
 
-def mailgun(fr, to, subject, body, mime_type):
-  url = os.getenv("MAILGUN_URL")
+
+def _post_json(url, data, basic_auth=None):
+  import urllib.request
+  jsonbytes = json.dumps(data).encode("utf-8")
+  req = urllib.request.Request(url, data=jsonbytes, method='POST')
+  req.add_header('Content-Type', 'application/json; charset=utf-8')
+  req.add_header('Content-Length', len(jsonbytes))
+  if basic_auth:
+    req.add_header('Authorization', f'Basic {basic_auth}')
+  result = urllib.request.urlopen(req)
+
+
+def mailjet(fr, to, subject, body, mime_type, attachments=None):
+  url = os.getenv("MAILJET_URL") or MAILJET_API_V3
+  basic_auth = b64encode(
+      (os.getenv("MAILJET_AUTH") or "").encode("ascii")).decode("ascii")
+  data = {
+      "FromEmail": fr,
+      "FromName": os.getenv("MAIL_FROM_NAME") or "howard",
+      "Subject": subject,
+      "To": ','.join(f"{addr.split('@')[0]} <{addr}>" for addr in to),
+  }
+  if mime_type == "html":
+    data["Html-part"] = body
+  else:
+    data["Text-part"] = body
+  if attachments:
+    data["Attachments"] = []
+    for filename in attachments:
+      with open(filename, "rb") as f:
+        a, b = mimetypes.guess_type(filename)
+        fmime = ""
+        if a is None or b is None:
+          fmime = "application/octet-stream"
+        else:
+          fmime = f"{a}/{b}"
+        data["Attachments"].append({
+            "Content": b64encode(f.read()).decode("ascii"),
+            "Content-type": fmime,
+            "Filename": filename,
+        })
+  _post_json(url, data, basic_auth)
+
 
 def smtp(fr, to, subject, body, mime_type, attachments=None):
   server = smtplib.SMTP_SSL(os.getenv("SMTP_HOST"),
@@ -23,16 +69,20 @@ def smtp(fr, to, subject, body, mime_type, attachments=None):
   server.login(os.getenv("SMTP_USER"), os.getenv("SMTP_PASS"))
   msg = MIMEMultipart()
   text = MIMEText(body, mime_type, "utf-8")
-  msg['From'] = _format_addr(f"{os.getenv('MAIL_FROM_NAME') or 'howard'} <{fr}>")
-  msg['To'] = ','.join(_format_addr(f"{addr.split('@')[0]} <{addr}>") for addr in to)
+  msg['From'] = _format_addr(
+      f"{os.getenv('MAIL_FROM_NAME') or 'howard'} <{fr}>")
+  msg['To'] = ','.join(_format_addr(
+      f"{addr.split('@')[0]} <{addr}>") for addr in to)
   msg['Subject'] = Header(subject, "utf-8").encode()
   msg.attach(text)
   if attachments:
     for i, attachment in enumerate(attachments):
       try:
         with open(attachment, "rb") as f:
-          mime = MIMEBase(*mimetypes.guess_type(attachment), filename=attachment)
-          mime.add_header('Content-Disposition', 'attachment', filename=attachment)
+          mime = MIMEBase(*mimetypes.guess_type(attachment),
+                          filename=attachment)
+          mime.add_header('Content-Disposition',
+                          'attachment', filename=attachment)
           mime.add_header('Content-ID', f'<{i}>')
           mime.add_header('X-Attachment-Id', f'{i}')
           mime.set_payload(f.read())
@@ -56,11 +106,13 @@ if __name__ == "__main__":
       "--html", help="Send the email as HTML", action="store_true")
   parser.add_argument("--retry", type=int, default=3,
                       help="Number of max retries")
-  parser.add_argument("--attach", type=str, action="append", nargs="*", help="Attatch a file")
+  parser.add_argument("--attach", type=str, action="append",
+                      nargs="*", help="Attatch a file")
   parser.add_argument("--via", type=str, default="smtp",
                       help="Send via SMTP or Mailgun")
 
   args = parser.parse_args()
+
   body = args.body
   if args.body_file:
     with open(args.body_file, "r", encoding="utf-8") as f:
@@ -75,3 +127,5 @@ if __name__ == "__main__":
     args.attach = [fn for arr in args.attach for fn in arr]
   if args.via == "smtp":
     smtp(from_addr, recipients, args.subject, body, mime_type, args.attach)
+  elif args.via == "mailjet":
+    mailjet(from_addr, recipients, args.subject, body, mime_type, args.attach)
