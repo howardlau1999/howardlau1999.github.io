abd164b4f92dcb2bc7e0de0745b5e4361a916deb
Howard Lau <howardlau1999@hotmail.com>
2022-06-21 19:55:28 +0800

    bar

--- tools/plot.py
+++ tools/plot.py
@@ -1,3 +1,4 @@
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -5,10 +6,12 @@ plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams['savefig.dpi'] = 300
 
+
 def _clear_legend(leg):
   leg.get_frame().set_edgecolor('b')
   leg.get_frame().set_linewidth(0.0)
   leg.get_frame().set_facecolor('none')
+
 
 x = np.linspace(-2 * np.pi, 2 * np.pi, 25)
 y = np.sin(x)
@@ -19,8 +22,9 @@ ax = axd["A"]
 ax.set_xlabel('x label')
 ax.set_ylabel('y label')
 ax.set_title("Simple Plot")
-ax.plot(x, y, 'o', markersize=3, label="sin(x)", color='black', linewidth=1, linestyle='--')
-ax.plot(x, cosy, 'v', markersize=3, label="cos(x)", 
+ax.plot(x, y, 'o', markersize=3, label="sin(x)",
+        color='black', linewidth=1, linestyle='--')
+ax.plot(x, cosy, 'v', markersize=3, label="cos(x)",
         color='black', linewidth=1, linestyle='--')
 ax.text(2.2, -1.5, r'$\mu=115,\ \sigma=15$')
 ax.annotate('local max', xy=(2, 1), xytext=(3, 1.5),
@@ -35,9 +39,19 @@ ax.set_ylabel('y label')
 ax.set_title("Simple Plot")
 
 # / , \\ , | , - , + , x, o, O, ., *
-import matplotlib.patches as mpatches
-ax.bar([1, 2, 3], [1, 2, 3], width=1, fill=False, hatch='/', tick_label="a")
-leg = ax.legend(handles=[mpatches.Patch(label='a', hatch='////', fill=False)])
+ax.xaxis.set_tick_params(length=0)
+ax.set_xlim(-2, 10)
+ax.bar([1, 2, 3], [1, 2, 3], width=1, fill=False,
+       hatch=['ooo', 'x', '...'])
+ax.bar([5, 6, 7], [1, 2, 3], width=1, fill=False,
+       hatch=['ooo', 'x', '...'])
+ax.set_xticks([2, 6])
+ax.set_xticklabels(['Write', 'Read'])
+leg = ax.legend(handles=[
+        mpatches.Patch(label='sqlite', hatch='ooo', fill=False), 
+        mpatches.Patch(label='dqlite-raft', hatch='xx', fill=False),
+        mpatches.Patch(label='dqlite-dpaxos', hatch='...', fill=False),
+], loc=2)
 _clear_legend(leg)
 
 plt.savefig("plot.png", bbox_inches='tight')
