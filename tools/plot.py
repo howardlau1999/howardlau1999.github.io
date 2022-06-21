40a5774a897c78649d80513e5482d8d537962d23
Howard Lau <howardlau1999@hotmail.com>
2022-06-21 22:16:59 +0800

    Update plot.py

--- tools/plot.py
+++ tools/plot.py
@@ -5,6 +5,7 @@ import numpy as np
 plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams['savefig.dpi'] = 300
+plt.rcParams["errorbar.capsize"] = 4
 
 
 def _clear_legend(leg):
@@ -34,24 +35,25 @@ leg = ax.legend()
 _clear_legend(leg)
 
 ax = axd["B"]
-ax.set_xlabel('x label')
-ax.set_ylabel('y label')
-ax.set_title("Simple Plot")
+ax.set_xlabel('Transaction Type')
+ax.set_ylabel('Avg. Latency (ms)')
+ax.set_title("")
 
 # / , \\ , | , - , + , x, o, O, ., *
 ax.xaxis.set_tick_params(length=0)
-ax.set_xlim(-2, 10)
-ax.bar([1, 2, 3], [1, 2, 3], width=1, fill=False,
-       hatch=['ooo', 'x', '...'])
-ax.bar([5, 6, 7], [1, 2, 3], width=1, fill=False,
-       hatch=['ooo', 'x', '...'])
+ax.set_xlim(0, 8)
+ax.bar([1, 2, 3], [1.64, 2.64, 2.14], width=1, fill=False, yerr=[[0.24, 1.58, 1.26], [0.18, 0.84, 0.66]], error_kw=dict(elinewidth=1),
+       hatch=['ooo', 'xxx', '...'])
+ax.bar([5, 6, 7], [1.21, 2.35, 2.04], width=1, fill=False, yerr=[[0.12, 0.78, 0.68], [0.08, 0.54, 0.44]], error_kw=dict(elinewidth=1),
+       hatch=['ooo', 'xxx', '...'], )
 ax.set_xticks([2, 6])
-ax.set_xticklabels(['Write', 'Read'])
+ax.set_yticks(np.linspace(0, 3, 5))
+ax.set_xticklabels(['read-write', 'read-only'])
 leg = ax.legend(handles=[
-        mpatches.Patch(label='sqlite', hatch='ooo', fill=False), 
-        mpatches.Patch(label='dqlite-raft', hatch='xx', fill=False),
-        mpatches.Patch(label='dqlite-dpaxos', hatch='...', fill=False),
-], loc=2)
+    mpatches.Patch(label='sqlite', hatch='ooo', fill=False),
+    mpatches.Patch(label='dqlite-raft', hatch='xxx', fill=False),
+    mpatches.Patch(label='dqlite-dpaxos', hatch='...', fill=False),
+], loc='lower left', bbox_to_anchor=(0, 1), ncol=3)
 _clear_legend(leg)
 
-plt.savefig("plot.png", bbox_inches='tight')
+plt.savefig("plot.pdf", bbox_inches='tight')
